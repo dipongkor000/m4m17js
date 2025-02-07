8d8b435f7a784721911a65953c4ee1a384b1180b
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-07 23:17:16 +0600

    add file

--- first8.js
+++ first8.js
@@ -1,1 +1,14 @@
 // ternary --> three parts
+
+// const age = 20;
+// if(age >= 18){
+//     console.log("you can vote.");
+// }
+// else{
+//     console.log('you can not vote.')
+// }
+
+// alternative rule 
+
+// const age = 12;
+// age >= 18 ? console.log('you can vote') : console.log('you can not vote')
